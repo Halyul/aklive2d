2c605093ebba70b3b0b820c2a071ab2d14aa57a5
Haoyu Xu <i@halyul.dev>
2022-03-29 13:34:21 -0400

    feat: add phatom and move to spine 3.8.99

---
+++ operator/phatom_placeholder/char_250_phatom_sale#4_settings.js
@@ -0,0 +1,51 @@
+let isPlayingInteract = false;
+let resetTime = window.performance.now();
+window.operatorSettings = {
+    fallbackImage: {
+        width: 2048,
+        height: 2048,
+    },
+    viewport: {
+        debugRender: false,
+        padLeft: "2%",
+        padRight: "2%",
+        padTop: "3%",
+        padBottom: "5%",
+        x: 0,
+        y: 0,
+    },
+    fps: 60,
+    animation: "Idle",
+    // jsonUrl: "./operator/dyn_illust_char_250_phatom_sale#4.json",
+    skelUrl: "./operator/dyn_illust_char_250_phatom_sale#4.skel",
+    atlasUrl: "./operator/dyn_illust_char_250_phatom_sale#4.atlas",
+    defaultMix: 0,
+    success: function (widget) {
+        settings.disableInvertFilter();
+        settings.opacityLogo(30);
+        widget.animationState.addListener({
+            end: (e) => {
+                if (e.animation.name == "Interact") {
+                    isPlayingInteract = false;
+                }
+            },
+            complete: (e) => {
+                if (window.performance.now() - resetTime >= 8 * 1000 && Math.random() < 0.3) {
+                    resetTime = window.performance.now();
+                    let entry = widget.animationState.setAnimation(0, "Special", false, 0);
+                    entry.mixDuration = 0.3;
+                    widget.animationState.addAnimation(0, "Idle", true, 0);
+                }
+            },
+        });
+        widget.canvas.onclick = function () {
+            if (isPlayingInteract) {
+                return;
+            }
+            isPlayingInteract = true;
+            let entry = widget.animationState.setAnimation(0, "Interact", false, 0);
+            entry.mixDuration = 0.3;
+            widget.animationState.addAnimation(0, "Idle", true, 0);
+        }
+    },
+}

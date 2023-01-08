4d1d57879e256b0cd44f21738b8c31399fa564a8
Haoyu Xu <i@halyul.dev>
2023-01-08 07:00:09 -0500

    feat(showcase): rewrite the webpage using preact for future development (1/3)

---
+++ template/showcase/src/app.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect, useRef } from 'preact/hooks'
+import '@/app.css'
+import '@/libs/wallpaper_engine'
+import logo from 'logo'
+import check_web_gl from '@/libs/check_web_gl'
+import Player from '@/components/player'
+import Fallback from '@/components/fallback'
+import Settings from '@/components/settings'
+
+export function App() {
+  const params = new URLSearchParams(window.location.search);
+  const isSupportWebGL = check_web_gl()
+  const logoRef = useRef(null);
+  const [spinePlayer, setSpinePlayer] = useState(null);
+
+  const [showControls, setShowControls] = useState(params.has("controls"));
+  const [showSettings, setShowSettings] = useState(params.has("settings"));
+
+  useEffect(() => {
+    document.title = import.meta.env.VITE_TITLE
+    console.log("All resources are extracted from Arknights. Github: https://github.com/Halyul/aklive2d")
+  }, []);
+
+  return (
+    <>
+      <img src={logo} class="logo invert-filter" id="logo" alt="operator logo" ref={logoRef} width={16} />
+      <Settings
+        spinePlayer={spinePlayer}
+        setShowSettings={setShowSettings}
+        hidden={!showSettings}
+        logoEl={logoRef.current}
+      />
+      <div id="widget-wrapper">
+        {
+          !isSupportWebGL ? (
+            <Fallback />
+          ) : (
+            <Player
+              showControls={showControls}
+              setSpinePlayer={setSpinePlayer}
+            />
+          )
+        }
+      </div>
+    </>
+  )
+}

580ad1d18d47e595257de84d29b6093da34fe7d4
Haoyu Xu <i@halyul.dev>
2023-01-20 15:24:20 -0500

    feat(initializer): update the initializer to change template content

--- libs/initializer.js
+++ libs/initializer.js
@@ -1,9 +1,15 @@
 import path from 'path'
-import { mkdir, copy } from './file.js'
+import { stringify } from 'yaml'
+import { read as readYAML } from './yaml.js'
+import { mkdir, writeSync } from './file.js'
 import { appendMainConfig } from './append.js'
 
 export default function init(extractedDir) {
     mkdir(extractedDir)
-    copy(path.join(__dirname, 'config', '_template.yaml'), path.join(__dirname, 'config', `${__operator_name}.yaml`))
+    const date = new Date()
+    const template = readYAML(path.join(__dirname, 'config', '_template.yaml'))
+    template.link = __operator_name
+    template.date = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0') }`
+    writeSync(stringify(template), path.join(__dirname, 'config', `${__operator_name}.yaml`))
     appendMainConfig(__operator_name, __dirname)
 }

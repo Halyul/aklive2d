660e4073c4a2b5a2000dddda06729a17d96b8b81
Halyul <i@halyul.dev>
2023-06-24 09:40:32 -0400

    chore(music): update music table

--- libs/music.js
+++ libs/music.js
@@ -8,13 +8,12 @@ export default class Music {
   #sharedPath = path.join(__projectRoot, __config.folder.operator, __config.folder.share)
 
   async process() {
-
+    await this.#download()
   }
 
   async #download() {
     const metaTable = await this.#downloader.github(`https://api.github.com/repos/Kengxxiao/ArknightsGameData/commits?path=zh_CN/gamedata/excel/display_meta_table.json`, `https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/display_meta_table.json`, path.join(this.#sharedPath, `display_meta_table.json`))
     const audioDataTable = await this.#downloader.github(`https://api.github.com/repos/Kengxxiao/ArknightsGameData/commits?path=zh_CN/gamedata/excel/audio_data.json`, `https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/audio_data.json`, path.join(this.#sharedPath, `audio_data.json`))
-    console.log(metaTable, audioDataTable)
   }
 
   copy() {

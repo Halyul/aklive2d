d7fd23fd546e0a94dd214e9272b96351060e8e9e
Haoyu Xu <i@halyul.dev>
2023-11-01 18:06:21 -0400

    feat(aklive2d): use offical_update to fetch info

--- libs/initializer.js
+++ libs/initializer.js
@@ -5,13 +5,19 @@ import { read as readYAML } from './yaml.js'
 import { mkdir, writeSync } from './file.js'
 import { appendMainConfig } from './append.js'
 
-export default function init(operatorName, extractedDir) {
+export default function init(operatorName, extractedDir, officalInfo) {
+    const officalId = process.argv[4]
+    const template = readYAML(path.join(__projectRoot, 'config', '_template.yaml'))
     extractedDir.forEach((dir) => {
         mkdir(dir)
     })
-    const date = new Date()
-    const template = readYAML(path.join(__projectRoot, 'config', '_template.yaml'))
-    template.date = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0') }`
+    const currentOpertor = officalInfo.find(officalId);
+    if (currentOpertor === undefined) {
+        throw new Error('Invalid operator id')
+    }
+    template.offical_id = currentOpertor.id
+    template.codename = currentOpertor.codename
+    
     writeSync(stringify(template), path.join(__projectRoot, 'config', `${operatorName}.yaml`))
     appendMainConfig(operatorName)
 }

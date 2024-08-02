b9f17a6e80beffdd67daa67496deb9686a101264
Haoyu Xu <i@halyul.dev>
2024-08-02 14:52:56 +0800

    fix(aklive2d): fixed init commented config

--- libs/append.js
+++ libs/append.js
@@ -4,7 +4,7 @@ import { appendSync } from './file.js'
 
 export function appendMainConfig(operatorName) {
     appendSync(
-        `\n  # ${operatorName}: !include config/${operatorName}.yaml`,
+        `\n  ${operatorName}: !include config/${operatorName}.yaml`,
         path.join(__projectRoot, 'config.yaml')
     )
 }
